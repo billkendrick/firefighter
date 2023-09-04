60720a110f3abf3add9b5d76e18de08934389da1
Bill Kendrick <nbs@sonic.net>
2023-09-04 12:36:47 -0700

    WIP Adding support for prev-page in Help
    For now, just wrapping new stuff in non-activated #ifdef

--- src/help.c
+++ src/help.c
@@ -15,17 +15,31 @@
 #include "help.h"
 #include "draw_text.h"
 
+#ifdef FANCY_HELP_IO
 #define LINES 22
+#define MAX_PAGES 50 // Should be plenty? Check ATASCII "README.txt"'s output length, divided by LINES
+#else
+#define LINES 23
+#endif
 
 enum {
   HELP_CMD_NONE,
   HELP_CMD_NEXT,
+#ifdef FANCY_HELP_IO
   HELP_CMD_PREV,
+#endif
   HELP_CMD_EXIT,
 };
 
 extern unsigned char scr_mem[];
 extern unsigned char * dlist;
+
+/* Local function prototypes: */
+#ifdef FANCY_HELP_IO
+void ciov(void);
+void note(unsigned char * ptr);
+void point(unsigned char * ptr);
+#endif
 
 /* Routine to load and show help text on a fullscreen text display */
 void show_help(void) {
@@ -33,6 +47,10 @@ void show_help(void) {
   unsigned char y, last, cmd;
   FILE * fi;
   char str[41];
+#ifdef FANCY_HELP_IO
+  int cur_page;
+  unsigned char ptrs[MAX_PAGES * 3];
+#endif
 
   OS.sdmctl = 0;
 
@@ -68,12 +86,25 @@ void show_help(void) {
 
   OS.sdmctl = (DMACTL_PLAYFIELD_NORMAL | DMACTL_DMA_FETCH);
 
+  /* Show control instructions */
   draw_text("SPACE/RETURN/FIRE: Next Page - ESC: Exit", scr_mem + (LINES * 40));
+#ifdef FANCY_HELP_IO
   draw_text("UP/BACKSPACE: Previous Page", scr_mem + ((LINES + 1) * 40) + 7);
+#endif
 
   fi = fopen("README.TXT", "r");
   if (fi != NULL) {
+#ifdef FANCY_HELP_IO
+    cur_page = 0;
+#endif
+
+    /* -- Main help screen loop -- */
     do {
+#ifdef FANCY_HELP_IO
+      note(ptrs + (cur_page * 3));
+#endif
+
+      /* Display a screenful (page) of text */
       y = 0;
       do {
         fgets(str, sizeof(str), fi);
@@ -85,6 +116,11 @@ void show_help(void) {
         y++;
       } while (!feof(fi) && y < LINES);
 
+#ifdef FANCY_HELP_IO
+      cur_page++;
+      draw_number(cur_page, 2, scr_mem + ((LINES + 1) * 40));
+#endif
+
       /* (Eat input) */
       do {
       } while (OS.strig0 == 0 || OS.strig1 == 0 || CONSOL_START(GTIA_READ.consol) == 1);
@@ -95,18 +131,40 @@ void show_help(void) {
       do {
         if (OS.strig0 == 0 || OS.strig1 == 0 ||
             OS.ch == KEY_SPACE || OS.ch == KEY_RETURN) {
-          cmd = HELP_CMD_NEXT;
-        }
-        if (CONSOL_START(GTIA_READ.consol) == 1 || OS.ch == KEY_ESC) {
-          cmd = HELP_CMD_EXIT;
-        }
-        if (0) { // FIXME
-          cmd = HELP_CMD_PREV;
+          /* Show next page (or exit, if we're at the end) */
+          if (!feof(fi)) {
+            cmd = HELP_CMD_NEXT;
+          } else {
+            cmd = HELP_CMD_EXIT;
+          }
         }
 
+        if (CONSOL_START(GTIA_READ.consol) == 1 || OS.ch == KEY_ESC) {
+          /* Exit immediately */
+          cmd = HELP_CMD_EXIT;
+        }
+
+#ifdef FANCY_HELP_IO
+        if (OS.stick0 == 14 || OS.stick1 == 14 || OS.ch == KEY_DASH || OS.ch == (KEY_DASH | KEY_CTRL)) {
+          /* Go back a page */
+          if (cur_page > 1) {
+            cmd = HELP_CMD_PREV;
+          }
+        }
+#endif
       } while (cmd == HELP_CMD_NONE);
 
       bzero(scr_mem, 40 * LINES);
+
+
+#ifdef FANCY_HELP_IO
+      if (cmd == HELP_CMD_PREV) {
+        /* Go back a page */
+        cur_page -= 2;
+        point(ptrs + (cur_page * 3));
+      }
+#endif
+
     } while (!feof(fi) && cmd != HELP_CMD_EXIT);
     fclose(fi);
   }
@@ -116,4 +174,35 @@ void show_help(void) {
   } while (OS.strig0 == 0 || OS.strig1 == 0 || CONSOL_START(GTIA_READ.consol) == 1);
   OS.ch = KEY_NONE;
 }
+
+#ifdef FANCY_HELP_IO
+void note(unsigned char * ptr) {
+  OS.iocb[0].command = IOCB_NOTE;
+  OS.iocb[0].buffer = ptr;
+  OS.iocb[0].buflen = 3;
+  ciov();
+}
+
+void point(unsigned char * ptr) {
+  OS.iocb[0].command = IOCB_POINT;
+  OS.iocb[0].buffer = ptr;
+  OS.iocb[0].buflen = 3;
+  ciov();
+}
+
+/*
+cio.s
+        ;; Call CIO
+
+        .export _ciov
+
+_ciov:  LDX #$00
+        JSR $E456
+        RTS
+*/
+void ciov(void) {
+  asm("LDX #$00"); // IOCB #
+  asm("JSR $E456");
+}
+#endif
 
