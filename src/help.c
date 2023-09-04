b08a655f6c0a5449b772c0b518fed55cd8add863
Bill Kendrick <nbs@sonic.net>
2023-09-04 11:57:26 -0700

    Some modularization to help.c

--- src/help.c
+++ src/help.c
@@ -5,7 +5,7 @@
   Bill Kendrick <bill@newbreedsoftware.com>
   http://www.newbreedsoftware.com/firefighter/
 
-  2023-08-22 - 2023-08-22
+  2023-08-22 - 2023-09-04
 */
 
 #include <atari.h>
@@ -15,13 +15,22 @@
 #include "help.h"
 #include "draw_text.h"
 
+#define LINES 22
+
+enum {
+  HELP_CMD_NONE,
+  HELP_CMD_NEXT,
+  HELP_CMD_PREV,
+  HELP_CMD_EXIT,
+};
+
 extern unsigned char scr_mem[];
 extern unsigned char * dlist;
 
 /* Routine to load and show help text on a fullscreen text display */
 void show_help(void) {
-  int i;
-  unsigned char y, last;
+  int i, j;
+  unsigned char y, last, cmd;
   FILE * fi;
   char str[41];
 
@@ -37,15 +46,17 @@ void show_help(void) {
   POKE(dlist + 3, DL_LMS(DL_GRAPHICS0));
   POKEW(dlist + 4, (unsigned int) scr_mem);
 
-  for (i = 6; i < 28; i++) {
+  for (i = 6; i < 6 + LINES - 1; i++) {
     POKE(dlist + i, DL_GRAPHICS0);
   }
 
-  POKE(dlist + 28, DL_BLK1);
-  POKE(dlist + 29, DL_GRAPHICS0);
+  POKE(dlist + i++, DL_BLK4);
 
-  POKE(dlist + 30, DL_JVB);
-  POKEW(dlist + 31, (unsigned int) dlist);
+  for (j = LINES; j < 24; j++)
+    POKE(dlist + i++, DL_GRAPHICS0);
+
+  POKE(dlist + i++, DL_JVB);
+  POKEW(dlist + i++, (unsigned int) dlist);
 
   OS.sdlst = dlist;
 
@@ -57,10 +68,8 @@ void show_help(void) {
 
   OS.sdmctl = (DMACTL_PLAYFIELD_NORMAL | DMACTL_DMA_FETCH);
 
-  do {
-  } while (OS.strig0 == 0 || OS.strig1 == 0 || CONSOL_START(GTIA_READ.consol) == 1);
-
-  draw_text("SPACE/FIRE: Continue - ESCAPE: Exit", scr_mem + (23 * 40) + 2);
+  draw_text("SPACE/RETURN/FIRE: Next Page - ESC: Exit", scr_mem + (LINES * 40));
+  draw_text("UP/BACKSPACE: Previous Page", scr_mem + ((LINES + 1) * 40) + 7);
 
   fi = fopen("README.TXT", "r");
   if (fi != NULL) {
@@ -74,20 +83,37 @@ void show_help(void) {
         }
         draw_text(str, scr_mem + y * 40);
         y++;
-      } while (!feof(fi) && y < 23);
+      } while (!feof(fi) && y < LINES);
 
-      OS.ch = KEY_NONE;
+      /* (Eat input) */
       do {
-      } while (OS.strig0 == 1 && OS.strig1 == 1 && CONSOL_START(GTIA_READ.consol) == 0 && OS.ch != KEY_SPACE && OS.ch != KEY_ESC);
+      } while (OS.strig0 == 0 || OS.strig1 == 0 || CONSOL_START(GTIA_READ.consol) == 1);
+      OS.ch = KEY_NONE;
 
-      bzero(scr_mem, 40 * 23);
-    } while (!feof(fi) && CONSOL_START(GTIA_READ.consol) == 0 && OS.ch != KEY_ESC);
+      /* Wait for input */
+      cmd = HELP_CMD_NONE;
+      do {
+        if (OS.strig0 == 0 || OS.strig1 == 0 ||
+            OS.ch == KEY_SPACE || OS.ch == KEY_RETURN) {
+          cmd = HELP_CMD_NEXT;
+        }
+        if (CONSOL_START(GTIA_READ.consol) == 1 || OS.ch == KEY_ESC) {
+          cmd = HELP_CMD_EXIT;
+        }
+        if (0) { // FIXME
+          cmd = HELP_CMD_PREV;
+        }
+
+      } while (cmd == HELP_CMD_NONE);
+
+      bzero(scr_mem, 40 * LINES);
+    } while (!feof(fi) && cmd != HELP_CMD_EXIT);
     fclose(fi);
   }
 
-  OS.ch = KEY_NONE;
-
+  /* (Eat input) */
   do {
   } while (OS.strig0 == 0 || OS.strig1 == 0 || CONSOL_START(GTIA_READ.consol) == 1);
+  OS.ch = KEY_NONE;
 }
 
